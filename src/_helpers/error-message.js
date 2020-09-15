9bd0fda6b2984cece46dacdaa35f4ac628a40d16
Igor Vasilyev <42575177+vis-86@users.noreply.github.com>
2020-09-15 11:07:34 +0300

    Update error-message.js

--- src/_helpers/error-message.js
+++ src/_helpers/error-message.js
@@ -5,7 +5,7 @@ export function getErrorMessage(error) {
     if ("string" === typeof error || error instanceof String) {
         msg = error
     } else {
-        msg = error && error.message ? error.message : "Упс! Возникли проблемы. Попробуйте позже."
+        msg = error && error.message ? error.message : "Ups. Please try later"
     }
     return msg;
 }
